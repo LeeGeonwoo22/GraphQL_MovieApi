8b18bd174cf11f11d72c14b180a9fce129dbfe57
Geon Woo <kwoo.chris@gmail.com>
2022-10-17 14:46:31 +0900

    #3  ping/pong

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 import { ApolloServer, gql } from "apollo-server";
 
+
+
 const typeDefs = gql`
   type User {
     id: ID
@@ -8,19 +10,30 @@ const typeDefs = gql`
   type Tweet {
     id: ID!
     text: String!
-    author: User!
+    author: User
   }
   type Query {
     allTweets: [Tweet!]!
-    tweet(id: ID): Tweet
+    tweet(id: ID!): Tweet
+    ping : String!
   }
   type Mutation {
     postTweet(text : String!, userId :ID!) : Tweet!
     deleteTweet(id : ID!) : Boolean!
   }
 `;
-
-const server = new ApolloServer({ typeDefs });
+const resolvers = {
+    Query : {
+      tweet() {
+        console.log("I'm called");
+        return null
+      },
+      ping(){
+        return "pong"
+      }
+    }
+}
+const server = new ApolloServer({ typeDefs, resolvers });
 
 server.listen().then(({ url }) => {
     console.log(`Running on ${url}`)
